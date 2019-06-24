324321b000aa61df4fc5a65d618e6b5096d4beff
Chaitanya Penjuri <chaitanya.penjuri95@gmail.com>
2019-06-25 01:53:07 +0530

    replaced tabbed activity for categories with empty one

--- app/src/main/java/com/chaitupenjudcoder/buckstrack/CategoriesActivity.java
+++ app/src/main/java/com/chaitupenjudcoder/buckstrack/CategoriesActivity.java
@@ -1,21 +1,7 @@
 package com.chaitupenjudcoder.buckstrack;
 
-import android.content.DialogInterface;
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.design.widget.TabLayout;
-import android.support.v4.view.ViewPager;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.LinearLayout;
-import android.widget.Toast;
-
-import com.chaitupenjudcoder.buckstrack.ui.main.SectionsPagerAdapter;
+import android.os.Bundle;
 
 public class CategoriesActivity extends AppCompatActivity {
 
@@ -23,40 +9,5 @@ public class CategoriesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_categories);
-        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
-        ViewPager viewPager = findViewById(R.id.view_pager);
-        viewPager.setAdapter(sectionsPagerAdapter);
-        TabLayout tabs = findViewById(R.id.tabs);
-        tabs.setupWithViewPager(viewPager);
-        FloatingActionButton fab = findViewById(R.id.fab);
-
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-                withEditText(view);
-            }
-        });
-    }
-
-    public void withEditText(View view) {
-
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("With Edit Text");
-
-        final EditText input = new EditText(CategoriesActivity.this);
-        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.MATCH_PARENT,
-                LinearLayout.LayoutParams.MATCH_PARENT);
-        input.setLayoutParams(lp);
-        builder.setView(input);
-        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                Toast.makeText(getApplicationContext(), "Text entered is " + input.getText().toString(), Toast.LENGTH_SHORT).show();
-            }
-        });
-        builder.show();
     }
 }
